7ba14ba6e71e8d444ff0e149b66c0e61356f3e90
Aisha <aishar@uia.no>
2024-11-18 13:08:16 +0100

    kgmodel.py

--- oblig5/barnehage/kgmodel.py
+++ oblig5/barnehage/kgmodel.py
@@ -1,55 +1,44 @@
-# kgmodel.py
 from dataclasses import dataclass
 from typing import Optional
-# kgmodel.py
 
+@dataclass
 class Foresatt:
-    def __init__(self, foresatt_id, foresatt_navn, foresatt_adresse, foresatt_tlfnr, foresatt_pnr):
-        self.foresatt_id = foresatt_id
-        self.foresatt_navn = foresatt_navn
-        self.foresatt_adresse = foresatt_adresse
-        self.foresatt_tlfnr = foresatt_tlfnr
-        self.foresatt_pnr = foresatt_pnr
+    foresatt_id: int
+    foresatt_navn: str
+    foresatt_adresse: str
+    foresatt_tlfnr: str
+    foresatt_pnr: str
 
-
+@dataclass
 class Barn:
-    def __init__(self, barn_id, barn_pnr):
-        self.barn_id = barn_id
-        self.barn_pnr = barn_pnr
+    barn_id: int
+    barn_pnr: str
 
-
+@dataclass
 class Barnehage:
-    def __init__(self, barnehage_id, barnehage_navn, barnehage_antall_plasser, barnehage_ledige_plasser):
-        self.barnehage_id = barnehage_id
-        self.barnehage_navn = barnehage_navn
-        self.barnehage_antall_plasser = barnehage_antall_plasser
-        self.barnehage_ledige_plasser = barnehage_ledige_plasser
+    barnehage_id: int
+    barnehage_navn: str
+    barnehage_antall_plasser: int
+    barnehage_ledige_plasser: int
 
-
+@dataclass
 class Soknad:
-    def __init__(
-        self, foresatt_navn, foresatt_adresse, foresatt_tlfnr, foresatt_pnr,
-        foresatt_2_navn=None, foresatt_2_adresse=None, foresatt_2_tlfnr=None, foresatt_2_pnr=None,
-        barn_pnr=None, barn_2_pnr=None,
-        fr_barnevern=False, fr_sykd_familie=False, fr_sykd_barn=False, fr_annet=None,
-        liste_over_barnehager_prioritert_5=None, tidspunkt_oppstart=None,
-        sosken_i_barnehagen=False, brutto_inntekt=0
-    ):
-        self.foresatt_navn = foresatt_navn
-        self.foresatt_adresse = foresatt_adresse
-        self.foresatt_tlfnr = foresatt_tlfnr
-        self.foresatt_pnr = foresatt_pnr
-        self.foresatt_2_navn = foresatt_2_navn
-        self.foresatt_2_adresse = foresatt_2_adresse
-        self.foresatt_2_tlfnr = foresatt_2_tlfnr
-        self.foresatt_2_pnr = foresatt_2_pnr
-        self.barn_pnr = barn_pnr
-        self.barn_2_pnr = barn_2_pnr
-        self.fr_barnevern = fr_barnevern
-        self.fr_sykd_familie = fr_sykd_familie
-        self.fr_sykd_barn = fr_sykd_barn
-        self.fr_annet = fr_annet
-        self.liste_over_barnehager_prioritert_5 = liste_over_barnehager_prioritert_5
-        self.tidspunkt_oppstart = tidspunkt_oppstart
-        self.sosken_i_barnehagen = sosken_i_barnehagen
-        self.brutto_inntekt = brutto_inntekt
+    foresatt_navn: str
+    foresatt_adresse: str
+    foresatt_tlfnr: str
+    foresatt_pnr: str
+    foresatt_2_navn: Optional[str] = None
+    foresatt_2_adresse: Optional[str] = None
+    foresatt_2_tlfnr: Optional[str] = None
+    foresatt_2_pnr: Optional[str] = None
+    barn_pnr: Optional[str] = None
+    barn_2_pnr: Optional[str] = None
+    fr_barnevern: bool = False
+    fr_sykd_familie: bool = False
+    fr_sykd_barn: bool = False
+    fr_annet: Optional[str] = None
+    liste_over_barnehager_prioritert_5: Optional[str] = None
+    tidspunkt_oppstart: Optional[str] = None
+    sosken_i_barnehagen: bool = False
+    brutto_inntekt: int = 0
+
